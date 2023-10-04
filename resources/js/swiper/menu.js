02dcf68e4d64161c346b702e59da0023c9951179
Mahmoud Shawish <mahmoudshawish2020@gmail.com>
2023-10-04 16:19:55 +0300

    Add: start implementing the structure of the website

---
+++ resources/js/swiper/menu.js
@@ -0,0 +1,27 @@
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 5,
+        },
+    },
+});
